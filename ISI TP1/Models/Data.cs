85699c18dfd3e0724ed42583560b35897243eae1
That1GuyWSSC <33987876+That1GuyWSSC@users.noreply.github.com>
2024-10-25 01:22:58 +0100

    tp1 hopefully first and last commit

---
+++ ISI TP1/Models/Data.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+namespace ISI_TP1.Models;
+
+
+public class Book
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? _id { get; set; }
+
+    
+    public string product_name { get; set; } = null!;
+
+    public string price { get; set; }
+
+    public string stock_quantity { get; set; } = null!;
+
+    public string location { get; set; } = null!;
+
+    public string manufacturer { get; set; } = null!;
+
+    public string expiration_date { get; set; } = null!;
+
+    public string vending_machine_id { get; set; } = null!;
+
+    public string category { get; set; } = null!;
+
+
+
+}
---
+++ ISI TP1/Models/TP1DatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace ISI_TP1.Models
+{
+    public class TP1DatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string TP1CollectionName { get; set; } = null!;
+    }
+}
